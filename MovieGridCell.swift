c84229f863a48f6ce3e1bd3a5060324979767923
Allan <lordsofjupiters@gmail.com>
2020-03-02 08:16:08 -0800

    first commit

---
+++ MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Jenny  on 2/27/20.
+//  Copyright © 2020 Allan. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+}
